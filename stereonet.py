52a1b6c8bbeffc23c670c332c70f8716f7587d5f
madeleine-tan <97422443+madeleine-tan@users.noreply.github.com>
2022-08-18 18:17:49 -0400

    Update and rename SnakeRiver_Stereonet.py to stereonet.py

--- stereonet.py
+++ stereonet.py
@@ -1,3 +1,6 @@
+# Create equal-area lower-hemisphere stereonet and density contour stereonet
+# Uses mplstereonet by Joe Kington: https://pypi.org/project/mplstereonet/
+
 import numpy as np
 import mplstereonet
 import matplotlib.pyplot as plt
@@ -12,20 +15,27 @@ dips = np.array(df[:,1])
 fig = plt.figure(figsize=(8,8))
 ax = fig.add_subplot(111, projection='stereonet')
 ax.grid()
-ax.set_title('Snake River Stereonet', y=1.10, fontsize=15)
-#ax.plane(strikes, dips, c='k')
+ax.set_title('Stereonet', y=1.10, fontsize=15)
+ax.plane(strikes, dips, c='k')
 ax.pole(strikes, dips, c='k')
 ax.set_azimuth_ticks([])
-#strike, dip = mplstereonet.fit_girdle(strikes, dips)
-#ax.pole(strike, dip, c='r')
-#ax.plane(strike, dip, c='r', label='axial plane')
-ax.density_contourf(strikes, dips, measurement='poles', cmap='Reds')
-ax.set_title('Density contour of the Poles', y=1.10, fontsize=15)
+
+#finding axial plane and pole
+#strike, dip = mplstereonet.fit_girdle(strikes, dips) #find axial plane
+#ax.pole(strike, dip, c='r') #find pole of axial plane
+#ax.plane(strike, dip, c='r', label='axial plane') #find axial plane
+
+#finding density contour
+#ax.density_contourf(strikes, dips, measurement='poles', cmap='Reds')
+#ax.set_title('Density contour of the Poles', y=1.10, fontsize=15)
+
+#creating degree labels
 label = np.arange(0,360,45)
 labx= 0.5-0.55*np.cos(np.radians(label+90))
 laby= 0.5+0.55*np.sin(np.radians(label+90))
 for i in range(len(label)):
     ax.text(labx[i],laby[i],str(int(label[i]))+'\N{DEGREE SIGN}', transform=ax.transAxes, ha='center', va='center')
+    
 #ax.legend(loc=4)
 plt.savefig('stereonet3.pdf')
 plt.show()
